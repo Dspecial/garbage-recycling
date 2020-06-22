596f6e15b469f4b6bfc9fde3d2a811c3c465ee5e
dingxixi <dingxixi@techbloom.net>
2020-06-22 18:04:17 +0800

    静态修改

--- js/recycling.js
+++ js/recycling.js
@@ -3,7 +3,7 @@
 * @Email: dxxtalking@163.com
 * @Date:   2020-06-06 17:00:09
 * @Last Modified by:   dxx
-* @Last Modified time: 2020-06-09 17:00:55
+* @Last Modified time: 2020-06-22 18:00:49
 */
 $(function () {
   var swiper = new Swiper('.swiper-container', {
